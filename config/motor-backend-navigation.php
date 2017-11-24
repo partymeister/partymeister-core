c31236bae59e148b0ce03b605f09c5c63832f2f3
Reza Esmaili <github@dfox.info>
2017-11-24 01:33:49 +0100

    Remove empty navigation item

--- config/motor-backend-navigation.php
+++ config/motor-backend-navigation.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'items' => [
-        100 => [
-            'slug'        => 'partymeister',
-            'name'        => 'partymeister-core::backend/global.partymeister',
-            'icon'        => 'fa fa-home',
-            'route'       => null,
-            'roles'       => [ 'SuperAdmin' ],
-            'permissions' => [ 'partymeister.read' ],
-            'items'       => []
-        ],
-    ]
+    //'items' => [
+    //    100 => [
+    //        'slug'        => 'partymeister',
+    //        'name'        => 'partymeister-core::backend/global.partymeister',
+    //        'icon'        => 'fa fa-home',
+    //        'route'       => null,
+    //        'roles'       => [ 'SuperAdmin' ],
+    //        'permissions' => [ 'partymeister.read' ],
+    //        'items'       => []
+    //    ],
+    //]
 ];
