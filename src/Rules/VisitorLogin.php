cc6a2c712479d38e2ec2ddc548d78c17267f1b6d
Reza Esmaili <bitbucket@dfox.info>
2022-09-29 16:19:54 +0200

    Fix validation message (thanks to saga musix for pointing that out)

--- src/Rules/VisitorLogin.php
+++ src/Rules/VisitorLogin.php
@@ -42,6 +42,6 @@ class VisitorLogin implements Rule
      */
     public function message()
     {
-        return 'The validation error message.';
+        return 'The validation error message. Das muss so!';
     }
 }
