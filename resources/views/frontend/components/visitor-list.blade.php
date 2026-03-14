c7ffc654b2070cd3ea1c62b64144c802ccfe2387
Reza Esmaili <github@dfox.info>
2026-03-14 03:14:02 +0100

    Update flag-icon-css class names to flag-icons (fi fi-xx)

--- resources/views/frontend/components/visitor-list.blade.php
+++ resources/views/frontend/components/visitor-list.blade.php
@@ -11,7 +11,7 @@
 <div class="grid-x">
     @foreach ($visitors as $v)
         <div class="cell medium-6 small-12">
-            <span class="flag-icon flag-icon-{{strtolower($v->country_iso_3166_1)}}"></span>
+            <span class="fi fi-{{strtolower($v->country_iso_3166_1)}}"></span>
             {{$v->name}} @if ($v->group != '') / {{$v->group}} @endif @if ($v->is_remote) <i class="fa {{ $v->remote_type == 'Satellite' ? 'fa-satellite-dish' : 'fa-laptop-house' }}" title="{{ $v->remote_type }}"></i> @endif
         </div>
     @endforeach
