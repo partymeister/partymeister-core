f48cadbfd4aa5dfb828f4799a61bc4ab4a4cec51
Reza Esmaili <github@dfox.info>
2026-03-14 09:07:06 +0100

    Rewrite schedule component: plain Tailwind table and badges

--- resources/views/frontend/components/schedule-tw.blade.php
+++ resources/views/frontend/components/schedule-tw.blade.php
@@ -1,20 +1,20 @@
 @foreach ($days as $dayKey => $times)
     <h4>{{$dayKey}}</h4>
-    <table class="table">
+    <table class="w-full text-left text-sm">
         <tbody>
         @foreach ($times as $hourKey => $events)
             <tr>
-                <td class="align-top w-[10%]"><strong>{{$hourKey}}</strong></td>
-                <td>
-                    <table class="table mb-0 pb-0 [&_tbody]:border-none [&_tr]:border-b-0 [&_td]:pt-0 [&_td]:pb-1">
+                <td class="align-top w-[10%] px-4 py-3 border-t border-border text-text"><strong>{{$hourKey}}</strong></td>
+                <td class="px-4 py-3 border-t border-border">
+                    <table class="w-full text-left text-sm mb-0 pb-0 [&_tbody]:border-none [&_tr]:border-b-0 [&_td]:pt-0 [&_td]:pb-1">
                         <tbody>
                         @foreach($events as $event)
                             <tr>
                                 <td class="w-[100px] border-t-0">
-                                    <span class="badge"
+                                    <span class="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium"
                                           style="color: black;background-color: {{$event['web_color']}}">{{$event['type']}}</span>
                                 </td>
-                                <td class="border-t-0">
+                                <td class="border-t-0 text-text">
                                     {{$event['name']}}
                                 </td>
                             </tr>
