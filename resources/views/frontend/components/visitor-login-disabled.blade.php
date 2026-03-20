9d0bf82bfde2e950924b630e55df6e181e67b26e
Reza Esmaili <github@dfox.info>
2026-03-20 10:39:09 +0100

    Remove dead non-Tailwind frontend templates
    These legacy Blade templates are unused after the Tailwind migration.

--- resources/views/frontend/components/visitor-login-disabled.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="callout warning">
-    <h4>Patience please</h4>
-    <p>Login and registration are currently disabled. Please check back later!</p>
-</div>
--- resources/views/frontend/components/visitor-registration.blade.php
+++
@@ -1,6 +0,0 @@
-@if (!isset($visitor) || is_null($visitor))
-    <h4>Registration</h4>
-    {!! form_start($visitorRegistrationForm, ['id' => 'category-item']) !!}
-    {!! form_until($visitorRegistrationForm, 'submit') !!}
-    {!! form_end($visitorRegistrationForm) !!}
-@endif
